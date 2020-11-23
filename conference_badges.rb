429f3e86d1f5839dff8b05bc4fc527dd817a07ee
Marvin Reyes <reyemarv@gmail.com>
2020-11-23 02:45:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ end
 
 def assign_rooms(attendees)
   attendees.map.each.with_index(1) do |attendee, index|
-    "Hello #{attendee}! You'll be assigned to room #{index}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end 
